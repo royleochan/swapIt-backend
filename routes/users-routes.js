0050ed5890b43ff5a2564a429bd389de6cc3cff2
royleochan <royleochan@gmail.com>
2020-05-29 03:53:13 +0800

    Skeleton for user API

---
+++ routes/users-routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const usersControllers = require("../controllers/users-controller");
+
+router.get('/')
+
+router.post('/signup', )
+
+router.post('./login', )
+
+const router = express.Router();
