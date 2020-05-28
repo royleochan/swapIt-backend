efabaaabd3b83dd0075e67a26fb737846c9d45bb
royleochan <royleochan@gmail.com>
2020-05-29 03:10:40 +0800

    Error handling for unsupported routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const HttpError = require("./models/http-error");
+
 const productRoutes = require("./routes/products-routes");
 
 const app = express();
@@ -8,5 +10,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/products", productRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find route", 404);
+  return next(error);
+});
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+      return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'});
+  });
 
 app.listen(5000);
