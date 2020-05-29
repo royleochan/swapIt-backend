6d1cc5c974a5c2fa45eb8fd09882d136747d0614
royleochan <royleochan@gmail.com>
2020-05-29 17:35:33 +0800

    Set up mongoose and product schema

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const HttpError = require("./models/http-error");
 
@@ -17,11 +18,18 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-      return next(error);
-    }
-    res.status(error.code || 500)
-    res.json({message: error.message || 'An unknown error occurred!'});
-  });
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
-app.listen(5000);
+mongoose
+  .connect("mongodb+srv://Roy:8Qurm#-Hgtcu$u4@cluster0-h3gax.mongodb.net/swapit?retryWrites=true&w=majority")
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
