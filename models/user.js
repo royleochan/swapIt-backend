3a26b8591133049be9a85c3a2d5d6cf9e084e280
royleochan <royleochan@gmail.com>
2020-05-29 18:05:03 +0800

    Create user schema

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: { type: String, required: true },
+  name: { type: String, required: true },
+  profilePic: { type: String, required: true },
+  description: { type: String, required: true },
+  location: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
