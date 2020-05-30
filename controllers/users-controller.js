42ecd53ba34beaf95b59cc4942b60789e29a2fcc
royleochan <royleochan@gmail.com>
2020-05-30 18:04:25 +0800

    Fetching all users in database

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -3,7 +3,22 @@ const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
-const getUsers = (req, res, next) => {};
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find(
+      {},
+      "userName email name description location products"
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching users failed, please try again later",
+      500
+    );
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
+};
 
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
