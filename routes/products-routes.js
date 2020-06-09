4c36f9f4ac3f2ff504a2ff201e5aac23912bd33b
royleochan <royleochan@gmail.com>
2020-06-09 21:38:31 +0800

    Change search request to a get request

--- routes/products-routes.js
+++ routes/products-routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/:pid", productsControllers.getProductById);
 
-router.post("/search", productsControllers.searchForProducts);
+router.get("/search/:query", productsControllers.searchForProducts);
 
 router.get("/user/:uid", productsControllers.getProductsByUserId);
 
