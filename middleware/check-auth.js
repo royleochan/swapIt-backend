d95a305a9549afb01e843ce924333d4c23911ed6
royleochan <royleochan@gmail.com>
2020-06-25 00:24:01 +0800

    Create check auth middleware to check for token

---
+++ middleware/check-auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error("Authentication failed!");
+    }
+    const decodedToken = jwt.verify(token, `${process.env.SECRET_KEY}`);
+    req.userData = { userId: decodedToken.user._id };
+    console.log(decodedToken);
+    next();
+  } catch (err) {
+    console.log(err);
+    const error = new HttpError("Authentication failed!", 401);
+    return next(error);
+  }
+};
