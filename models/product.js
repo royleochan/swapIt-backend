71ed61a8f1eaf8d74434c9a1834ef3917766f810
royleochan <royleochan@gmail.com>
2021-01-04 19:09:21 +0800

    Change category to number

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ const productSchema = new Schema({
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   likes: [{ type: mongoose.Types.ObjectId, ref: "User" }],
   matches: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
-  category: { type: String, required: true },
+  category: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("Product", productSchema);
