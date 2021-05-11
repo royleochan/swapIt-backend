426cf05e5c47c28357e3faed09c21d5298d59a2b
pangpuncake <pangpuncake@gmail.com>
2021-05-11 18:04:11 +0800

    Update message.js to remove date

--- models/message.js
+++ models/message.js
@@ -5,8 +5,7 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
     creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
     content: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    date: { type: Date, required: true },
-}, { timestamps: true});
+    imageUrl: { type: String },
+}, { timestamps: true });
 
 module.exports = mongoose.model("Message", messageSchema);
