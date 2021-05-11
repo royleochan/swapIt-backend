5906431db97be0dcc759e2def330b8675f62d455
pangpuncake <pangpuncake@gmail.com>
2021-05-11 23:22:05 +0800

    Update chat backend

--- chatSocket.js
+++ chatSocket.js
@@ -3,19 +3,22 @@ const Message = require("./models/message");
 const User = require("./models/user");
 
 const createChatRoom = async (userId1, userId2) => {
-    const user1 = await User.findById(userId1);
-    const user2 = await User.findById(userId2);
+    try {
+        const user1 = await User.findById(userId1);
+        const user2 = await User.findById(userId2);
 
-    const createdChat = new Chat({
-        users: [user1, user2],
-        messages: [],
-    });
-    await createdChat.save();
+        const createdChat = new Chat({
+            users: [user1, user2],
+            messages: [],
+        });
+        await createdChat.save();
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 const chatSocket = (io) => {
     io.on("connection", (socket) => {
-        console.log("User connected");
         socket.on("join", async (roomId) => {
             try {
                 let result = await Chat.findById(roomId);
@@ -30,16 +33,20 @@ const chatSocket = (io) => {
             }
         });
         socket.on("message", async ({ userId, message, imageUrl }) => {
-            let chat = await Chat.findById(socket.activeRoom);
-            let msg = new Message({
-                creator: userId,
-                content: message,
-                imageUrl: imageUrl,
-            });
-            chat.messages.push(msg);
-            await msg.save();
-            await chat.save();
-            io.to(socket.activeRoom).emit("message", msg);
+            try {
+                let chat = await Chat.findById(socket.activeRoom);
+                let msg = new Message({
+                    creator: userId,
+                    content: message,
+                    imageUrl: imageUrl,
+                });
+                chat.messages.push(msg);
+                await msg.save();
+                await chat.save();
+                io.to(socket.activeRoom).emit("message", msg);
+            } catch (e) {
+                console.error(e);
+            }
         });
     });
 };
--- routes/chats-routes.js
+++ routes/chats-routes.js
@@ -5,6 +5,5 @@ const chatsController = require("../controllers/chats-controller");
 const router = express.Router();
 
 router.get("/:rid", chatsController.getChatRoomById);
-router.get("/", chatsController.getTestChat);
 
 module.exports = router;
