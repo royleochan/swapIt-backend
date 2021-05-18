751d90a24c7c3dab0f9709d6ddcb08986300fa7c
pangpuncake <pangpuncake@gmail.com>
2021-05-18 17:59:58 +0800

    Update message schema to allow image upload without content

--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
     creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-    content: { type: String, required: true },
+    content: { type: String, required: () => this.imageUrl === undefined },
     imageUrl: { type: String },
 }, { timestamps: true });
 
