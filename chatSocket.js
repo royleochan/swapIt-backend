cc19013eb2ed003f39efb3b30044810a7d8fb82f
pangpuncake <pangpuncake@gmail.com>
2021-05-18 18:55:23 +0800

    Revert previous commit

--- chatSocket.js
+++ chatSocket.js
@@ -1,205 +1,54 @@
-import uuid from "react-native-uuid";
-import { REACT_APP_BACKEND_URL } from "@env";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Actions, GiftedChat } from "react-native-gifted-chat";
-import { useSelector } from "react-redux";
-import { io } from "socket.io-client";
-import { Icon } from "react-native-elements";
+const Chat = require("./models/chat");
+const Message = require("./models/message");
+const User = require("./models/user");
 
-import request from "utils/request";
-import Colors from "constants/Colors";
-import {
-    takeImage,
-    chooseFromLibrary,
-    uploadImageHandler,
-} from "utils/imagePicker";
+const createChatRoom = async (userId1, userId2) => {
+    try {
+        const user1 = await User.findById(userId1);
+        const user2 = await User.findById(userId2);
 
-const ChatScreen = (props) => {
-    const [socket] = useState(
-        io(`${REACT_APP_BACKEND_URL}/chatSocket`, {
-            autoConnect: false,
-        })
-    );
-    const [messages, setMessages] = useState([]);
-    const [lastSentMessage, setLastSentMessage] = useState("");
-    const [lastSentImageUrl, setLastSentImageUrl] = useState("");
-
-    const loggedInUserId = useSelector((state) => state.auth.user.id);
-    const userId = props.route.params._id;
-    const userProfilePic = props.route.params.profilePic;
-
-    const updateMessages = (userId, content, imageUrl) => {
-        const newMessage = [
-            {
-                _id: uuid.v4(),
-                text: content,
-                image: imageUrl,
-                createdAt: new Date(),
-                user: {
-                    _id: userId,
-                    avatar: userProfilePic,
-                },
-            },
-        ];
-        setMessages((previousMessages) =>
-            GiftedChat.append(previousMessages, newMessage)
-        );
+        const createdChat = new Chat({
+            users: [user1, user2],
+            messages: [],
+        });
+        await createdChat.save();
+    } catch (e) {
+        console.error(e);
     }
-
-    const handleCancel = () => {};
-
-    //Choose image from image library
-    const handlePickImage = async () => {
-        try {
-            let img = await chooseFromLibrary();
-            if (isValidImage(img)) {
-                console.log("Uploading image...");
-                let imageUrl = await uploadImageHandler(img);
-                setValidImageUrl(imageUrl);
-            } else {
-                console.log("Image chosen is invalid!");
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    };
-
-    const isValidString = (inputString) => {
-        return inputString !== "" && inputString !== undefined;
-    };
-
-    const isValidImage = (img) => {
-        return img !== undefined;
-    };
-
-    const setValidImageUrl = (inputUrl) => {
-        if (isValidString(inputUrl)) {
-            setLastSentImageUrl(inputUrl);
-        } else {
-            console.log("Invalid Image Url:", inputUrl);
-        }
-    };
-
-    const renderActions = (props) => {
-        return (
-            <Actions
-                {...props}
-                options={{
-                    ["Use camera"]: takeImage,
-                    ["Choose Image"]: handlePickImage,
-                    ["Cancel"]: handleCancel,
-                }}
-                icon={() => (
-                    <Icon name={"attachment"} size={23} color={Colors.primary} />
-                )}
-                onSend={(args) => console.log(args)}
-            />
-        );
-    };
-
-    const getMessages = async (roomId) => {
-        //   const response = await request.get(`/api/chats/${loggedInUserId}/${userId}`);
-        const response = await request.get(`/api/chats/${roomId}`);
-        return response.data.room.messages.map((message) => {
-            return {
-                _id: message._id,
-                text: message.content,
-                image: message.imageUrl,
-                createdAt: message.createdAt,
-                user: {
-                    _id: message.creator,
-                    avatar: userProfilePic,
-                },
-            };
-        });
-    };
-
-    useEffect(() => {
-        // setIsLoading(true);
-        console.log("Attempting to connect...");
-        socket.connect();
-        socket.on("connect", async () => {
-            console.log("Received connect, preparing to emit...");
-            socket.emit("join", "609a8094dec46a7ce23a5e61");
-        });
-        socket.on("joined", async (roomId) => {
-            console.log("User has joined room %s", roomId);
-            await getMessages(roomId)
-                .then((response) => {
-                    // setIsLoading(false);
-                    setMessages(response.reverse());
-                })
-                .catch((e) => console.error(e));
-        });
-        socket.on("message", (message) => {
-            const newMessage = [
-                {
-                    _id: uuid.v4(),
-                    text: message.content,
-                    image: message.imageUrl,
-                    createdAt: new Date(),
-                    user: {
-                        _id: message.userId,
-                        avatar: userProfilePic,
-                    },
-                },
-            ];
-            setMessages((previousMessages) =>
-                GiftedChat.append(previousMessages, newMessage)
-            );
-        });
-        return () => {
-            console.log("DISCONNECT");
-            socket.disconnect();
-        };
-    }, []);
-
-    useEffect(() => {
-        if (isValidString(lastSentMessage)) {
-            console.log("LAST SENT:" + lastSentMessage);
-            socket.emit("message", {
-                userId: loggedInUserId,
-                message: lastSentMessage,
-                imageUrl: "",
-            });
-        } else {
-            console.log("Invalid message");
-        }
-    }, [lastSentMessage]);
-
-    useEffect(() => {
-        if (isValidString(lastSentImageUrl)) {
-            console.log("LAST SENT:" + lastSentImageUrl);
-            socket.emit("message", {
-                userId: loggedInUserId,
-                message: "",
-                imageUrl: lastSentImageUrl,
-            });
-            updateMessages(loggedInUserId, "", lastSentImageUrl);
-        } else {
-            console.log("Invalid Image Url");
-        }
-    }, [lastSentImageUrl]);
-
-    const onSend = useCallback((newMessage = []) => {
-        console.log(newMessage[0]);
-        setLastSentMessage(newMessage[0].text);
-        setMessages((previousMessages) =>
-            GiftedChat.append(previousMessages, newMessage)
-        );
-    }, []);
-
-    return (
-        <GiftedChat
-            messages={messages}
-            onSend={(messages) => onSend(messages)}
-            user={{
-                _id: loggedInUserId,
-            }}
-            renderActions={renderActions}
-            infiniteScroll
-        />
-    );
 };
 
-export default ChatScreen;
+const chatSocket = (io) => {
+    io.on("connection", (socket) => {
+        socket.on("join", async (roomId) => {
+            try {
+                let result = await Chat.findById(roomId);
+                if (!result) {
+                    await createChatRoom(roomId);
+                }
+                socket.join(roomId);
+                socket.emit("joined", roomId);
+                socket.activeRoom = roomId;
+            } catch (e) {
+                console.error(e);
+            }
+        });
+        socket.on("message", async ({ userId, message, imageUrl }) => {
+            try {
+                let chat = await Chat.findById(socket.activeRoom);
+                let msg = new Message({
+                    creator: userId,
+                    content: message,
+                    imageUrl: imageUrl,
+                });
+                chat.messages.push(msg);
+                await msg.save();
+                await chat.save();
+                socket.to(socket.activeRoom).emit("message", msg);
+            } catch (e) {
+                console.error(e);
+            }
+        });
+    });
+};
+
+module.exports = chatSocket;
