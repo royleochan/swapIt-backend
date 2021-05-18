d8b0c344b28355bc624f5d731ceb0662b03f7da2
pangpuncake <pangpuncake@gmail.com>
2021-05-19 00:49:36 +0800

    Add new route for chat controller

--- routes/chats-routes.js
+++ routes/chats-routes.js
@@ -4,6 +4,7 @@ const chatsController = require("../controllers/chats-controller");
 
 const router = express.Router();
 
+router.get("/rooms/:uid", chatsController.getAllChatRooms);
 router.get("/:rid", chatsController.getChatRoomById);
 router.get("/:uid1/:uid2", chatsController.findMatchingRoom);
 
