fccf9633fa549877bf88c19f1313a74557585d28
royleochan <royleochan@gmail.com>
2021-05-20 18:56:57 +0800

    Add timestamp for products and reviews

--- models/product.js
+++ models/product.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  minPrice: { type: Number, required: true },
-  maxPrice: { type: Number, required: true },
-  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-  likes: [{ type: mongoose.Types.ObjectId, ref: "User" }],
-  matches: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
-  category: { type: String, required: true },
-});
+const productSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    minPrice: { type: Number, required: true },
+    maxPrice: { type: Number, required: true },
+    creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    likes: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+    matches: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
+    category: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", productSchema);
--- models/review.js
+++ models/review.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
-  description: { type: String, required: true },
-  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" }, // the user writing the review
-  reviewed: { type: mongoose.Types.ObjectId, required: true, ref: "User" }, // the user being reviewed
-  rating: { type: Number, required: true },
-});
+const reviewSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" }, // the user writing the review
+    reviewed: { type: mongoose.Types.ObjectId, required: true, ref: "User" }, // the user being reviewed
+    rating: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Review", reviewSchema);
