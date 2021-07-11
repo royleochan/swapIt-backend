eeca7ec51ef16d7907fbeab26137fa774105d508
royleochan <royleochan@gmail.com>
2021-07-12 00:02:40 +0800

    Add notifs for request and accept

--- models/notification.js
+++ models/notification.js
@@ -7,7 +7,7 @@ const notificationSchema = new Schema(
     creator: { type: mongoose.Types.ObjectId, ref: "User" },
     targetUser: { type: mongoose.Types.ObjectId, ref: "User" },
     productId: { type: mongoose.Types.ObjectId, ref: "Product" },
-    matchedProductId: { type: mongoose.Types.ObjectId, ref: "Product" },
+    matchedProductId: { type: mongoose.Types.ObjectId, ref: "Product" }, // refers to OTHER PRODUCT ID, not necessarily matched yet, can be requested
     description: { type: String, required: true },
     type: { type: String, required: true },
     isRead: { type: Boolean, required: true, default: false },
