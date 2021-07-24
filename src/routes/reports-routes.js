7e199fc9b52d8401fb67effcabbbdee03077d512
royleochan <royleochan@gmail.com>
2021-07-24 14:27:42 +0800

    Add swagger doc for reports

--- src/routes/reports-routes.js
+++ src/routes/reports-routes.js
@@ -4,6 +4,36 @@ const reportControllers = require("../controllers/reports-controller");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *
+ * /reports/new:
+ *   post:
+ *     summary: Sends a report to user who made the report as well as SwapIt admin email
+ *     tags: [Email Report Service]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - subject
+ *               - email
+ *               - description
+ *             properties:
+ *               subject:
+ *                 type: string
+ *                 example: Bug
+ *               email:
+ *                 type: string
+ *                 example: leoroychan@gmail.com
+ *               description:
+ *                 type: string
+ *                 example: Faced bug where cannot upload product
+ *     responses:
+ *       "201":
+ *         description: The sent email information
+ */
 router.post("/new", reportControllers.createReport);
 
 module.exports = router;
